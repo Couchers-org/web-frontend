47899a379c17b5aab49ebc5f561cb4c15a89533a
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-05-12 17:55:34 +0200

    Build basic layout, image, and buttons of the dashboard cover

--- features/dashboard/Dashboard.tsx
+++ features/dashboard/Dashboard.tsx
@@ -13,6 +13,7 @@ import { DASHBOARD, GLOBAL } from "i18n/namespaces";
 import { blogRoute } from "routes";
 
 import CommunitiesSection from "./CommunitiesSection";
+import CoverBlock from "./CoverBlock/CoverBlock";
 import DashboardUserProfileSummary from "./DashboardUserProfileSummary";
 import MyEvents from "./MyEvents";
 
@@ -32,39 +33,47 @@ export default function Dashboard() {
   const classes = useStyles();
 
   return (
-    <Grid container direction="row">
-      <Grid item sm={4} xs={12} className={classes.profileOverviewContainer}>
-        <DashboardUserProfileSummary />
+    <>
+      <CoverBlock />
+
+      <Grid container direction="row">
+        <Grid item sm={4} xs={12} className={classes.profileOverviewContainer}>
+          <DashboardUserProfileSummary />
+        </Grid>
+
+        <Grid item sm={8} xs={12} className={classes.mainContentContainer}>
+          <HtmlMeta title={t("global:nav.dashboard")} />
+
+          <PageTitle>{t("dashboard:welcome")}</PageTitle>
+          <Typography variant="body1" paragraph>
+            <Trans i18nKey="dashboard:landing_text">
+              {`We are building new `}
+              <MuiLink
+                href={blogRoute}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                features
+              </MuiLink>
+              {` like events, local guides, moderation and hangouts. We appreciate your patience and support as we develop these.`}
+            </Trans>
+          </Typography>
+
+          <Typography variant="h1" component="h2" paragraph>
+            {t("dashboard:dashboard")}
+          </Typography>
+
+          <DashboardBanners />
+
+          <Divider spacing={3} />
+
+          <MyEvents />
+
+          <Divider spacing={3} />
+
+          <CommunitiesSection />
+        </Grid>
       </Grid>
-
-      <Grid item sm={8} xs={12} className={classes.mainContentContainer}>
-        <HtmlMeta title={t("global:nav.dashboard")} />
-
-        <PageTitle>{t("dashboard:welcome")}</PageTitle>
-        <Typography variant="body1" paragraph>
-          <Trans i18nKey="dashboard:landing_text">
-            {`We are building new `}
-            <MuiLink href={blogRoute} target="_blank" rel="noreferrer noopener">
-              features
-            </MuiLink>
-            {` like events, local guides, moderation and hangouts. We appreciate your patience and support as we develop these.`}
-          </Trans>
-        </Typography>
-
-        <Typography variant="h1" component="h2" paragraph>
-          {t("dashboard:dashboard")}
-        </Typography>
-
-        <DashboardBanners />
-
-        <Divider spacing={3} />
-
-        <MyEvents />
-
-        <Divider spacing={3} />
-
-        <CommunitiesSection />
-      </Grid>
-    </Grid>
+    </>
   );
 }
