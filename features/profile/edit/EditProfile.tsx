692f42c1839f065e8122246e5c11d52c519ffbad
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-06-02 10:04:44 +0200

    Make "Become a host" link take user to edit profile view to change hosting status

--- features/profile/edit/EditProfile.tsx
+++ features/profile/edit/EditProfile.tsx
@@ -230,7 +230,9 @@ export default function EditProfileForm() {
               name="hostingStatus"
               render={({ onChange, value }) => (
                 <>
-                  <Typography variant="h2">{HOSTING_STATUS}</Typography>
+                  <Typography variant="h2" id="hosting_status">
+                    {HOSTING_STATUS}
+                  </Typography>
                   <RadioGroup
                     row
                     aria-label={HOSTING_STATUS}
--- routes.ts
+++ routes.ts
@@ -58,6 +58,8 @@ export function routeToEditProfile(tab?: EditUserTab) {
   return `${profileBaseRoute}/edit${tab ? `/${tab}` : ""}`;
 }
 
+export const routeToBecomeAHost = `${routeToEditProfile()}#hosting_status`;
+
 // user
 const userBaseRoute = "/user";
 
