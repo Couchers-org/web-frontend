2f721cddbffa46878743b1864f203ae57fa0efb0
Will Ginsberg <will.j.ginsberg@gmail.com>
2022-10-26 11:21:28 -0500

    update LoginForm to use updated password login service

--- features/auth/login/Login.test.tsx
+++ features/auth/login/Login.test.tsx
@@ -6,15 +6,17 @@ import { assertErrorAlert, t } from "test/utils";
 
 import Login from "./Login";
 
-const checkUsernameMock = service.auth.checkUsername as jest.MockedFunction<
-  typeof service.auth.checkUsername
+const passwordLoginMock = service.user.passwordLogin as jest.MockedFunction<
+  typeof service.user.passwordLogin
 >;
 
-it("shows the known gRPC error from the API", async () => {
-  const errorMessage = "Couldn't find that user.";
-  checkUsernameMock.mockRejectedValue({
-    code: 5,
-    message: errorMessage,
+it("shows the known error from the API", async () => {
+  passwordLoginMock.mockRejectedValue({
+    error_messages: [
+      "Unable to log in with provided credentials."
+    ],
+    errors: {},
+    status_code: 400
   });
   render(<Login />, { wrapper });
 
@@ -22,16 +24,24 @@ it("shows the known gRPC error from the API", async () => {
     await screen.findByLabelText(
       t("auth:login_page.form.username_field_label")
     ),
-    "invalid"
+    "test-user"
   );
+
+  userEvent.type(
+    await screen.findByLabelText(
+      t("auth:login_page.form.password_field_label")
+    ),
+    "test-password"
+  );
+
   userEvent.click(screen.getByRole("button", { name: t("global:continue") }));
 
-  await assertErrorAlert(errorMessage);
+  await assertErrorAlert("Unable to log in with provided credentials");
 });
 
 it("shows the fatal error message for unknown errors", async () => {
-  checkUsernameMock.mockRejectedValue({
-    message: "unknown error",
+  passwordLoginMock.mockRejectedValue({
+    status_code: 500
   });
   render(<Login />, { wrapper });
 
@@ -39,8 +49,16 @@ it("shows the fatal error message for unknown errors", async () => {
     await screen.findByLabelText(
       t("auth:login_page.form.username_field_label")
     ),
-    "invalid"
+    "test-user"
   );
+
+  userEvent.type(
+    await screen.findByLabelText(
+      t("auth:login_page.form.password_field_label")
+    ),
+    "test-password"
+  );
+
   userEvent.click(screen.getByRole("button", { name: t("global:continue") }));
 
   await assertErrorAlert(t("global:error.fatal_message"));
